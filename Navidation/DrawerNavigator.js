7c56079ef26f5b925c3f4fca15532071247c55c8
addy360 <abbyboy360@gmail.com>
2021-06-19 17:00:15 +0300

    drawer bgc

--- Navidation/DrawerNavigator.js
+++ Navidation/DrawerNavigator.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../Screens/HomeScreen";
+import { appTheme } from "../constants";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerStyle={{ backgroundColor: appTheme.appColors.primaryColor }}
+      drawerType="slide"
+    >
       <Drawer.Screen name="Home" component={HomeScreen} />
     </Drawer.Navigator>
   );
